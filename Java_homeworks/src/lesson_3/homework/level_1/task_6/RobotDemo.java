cb7d0df71bce995605e8728b4dad468d57d1691f
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-06 19:40:01 +0300

    initial commit

--- Java_homeworks/src/lesson_3/homework/level_1/task_6/RobotDemo.java
+++ Java_homeworks/src/lesson_3/homework/level_1/task_6/RobotDemo.java
@@ -2,8 +2,12 @@ package lesson_3.homework.level_1.task_6;
 
 public class RobotDemo {
     public static void main(String[] args) {
-        Robot robot = new Robot("ROBO");
-        robot.sayHello();
-        robot.sayYourName();
+        Robot robot1 = new Robot("ROBO1");
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot("ROBO2");
+        robot2.sayHello();
+        robot2.sayYourName();
     }
 }
