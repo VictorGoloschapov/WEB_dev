c1723e25bd4116815aeb3eeb21934a2de276467d
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-06 18:13:45 +0300

    initial commit

---
+++ Java_homeworks/src/lesson_3/homework/level_1/task_2/Robot.java
@@ -0,0 +1,13 @@
+//Task_2
+package lesson_3.homework.level_1.task_2;
+
+public class Robot {
+
+    public Robot() {
+
+    }
+
+    void sayHello() {
+        System.out.println("Hello");
+    }
+}
