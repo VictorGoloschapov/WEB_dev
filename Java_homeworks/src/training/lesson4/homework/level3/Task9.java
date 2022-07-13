8c983b226101db464acebfbcf3e645a35039d53f
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-13 22:14:34 +0300

    initial commit

--- Java_homeworks/src/training/lesson4/homework/level3/Task9.java
+++ Java_homeworks/src/training/lesson4/homework/level3/Task9.java
@@ -1,6 +1,7 @@
 package training.lesson4.homework.level3;
 import java.util.Scanner;
-public class Task8 {
+
+public class Task9 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -17,12 +18,12 @@ public class Task8 {
         System.out.print("Введите целое число 3: ");
         number3 = input.nextInt();
 
-        if (number1 == number2 && number2 == number3) {
-            System.out.println("All numbers are equal");
-        } else if ((number1 == number2) || (number1 == number3) || (number2 == number3)) {
-            System.out.println("Neither all are equal or different");
+        if (number1 < number2 && number2 < number3) {
+            System.out.println("Increasing order");
+        } else if (number1 > number2 && number2 > number3) {
+            System.out.println("Decreasing order");
         } else {
-            System.out.println("All numbers are different");
+            System.out.println("Neither increasing or decreasing order");
         }
     }
 }
