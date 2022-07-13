9cfda0d4ae98aa1805e7cd14b7b28babb972dcc6
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-13 16:02:13 +0300

    initial commit

---
+++ Java_homeworks/src/training/IfElseStatement.java
@@ -0,0 +1,24 @@
+package training;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int x = 115;
+
+        if(x < 20) {
+            System.out.println("True output");
+        } else {
+            System.out.println("else code");
+        }
+
+        //if..else if...else
+        if(x <  20) {
+            System.out.println("Условие 1");
+        } else if(x > 30 && x < 115) {
+            System.out.println("Условие 2");
+        } else if (x == 5) {
+            System.out.println("Условие 3");
+        } else {
+            System.out.println("else");
+        }
+    }
+}
---
+++ Java_homeworks/src/training/IfStatement.java
@@ -0,0 +1,11 @@
+package training;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        int x = 10;
+
+        if(x < 20) {
+            System.out.println(x  + " less than 20");
+        }
+    }
+}
