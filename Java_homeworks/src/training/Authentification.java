e1e2b5ff7338a7c51d0a1194d5f0e85e0d0d66f6
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-13 20:55:51 +0300

    initial commit

--- Java_homeworks/src/training/Authentification.java
+++ Java_homeworks/src/training/Authentification.java
@@ -1,10 +1,11 @@
 package training;
+
 import java.util.Scanner;
 
 public class Authentification {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        final String userName = "Victor";
+        final String userName = "Olga1";
         final String password = "qwerty!!";
         String login;
         String userPass;
@@ -14,10 +15,15 @@ public class Authentification {
         System.out.print("Enter password: ");
         userPass = input.nextLine();
 
-        if((login.equals(userName)) && (userPass.equals(password))) {
-            System.out.print("login successful");
+        if ((!login.equals("")) && (!userPass.equals(""))) {
+            if ((login.equals(userName)) && (userPass.equals(password))) {
+                System.out.print("login successful");
+            } else {
+                System.out.print("login or password are incorrect");
+            }
         } else {
-            System.out.print("login or password are incorrect");
+            System.out.print("please enter a value");
         }
     }
 }
+
---
+++ Java_homeworks/src/training/lesson4/homework/level1/Task1.java
@@ -0,0 +1,23 @@
+package training.lesson4.homework.level1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Введите число ");
+
+        if (input.hasNextInt()) {
+            number = input.nextInt();
+            if (number < 0) {
+                System.out.println("Число " + number + " отрицательное");
+            } else {
+                System.out.println("Число " + number + " положительное");
+            }
+        } else {
+            System.out.println("Извините, но это явно не число. Введите число");
+        }
+    }
+}
