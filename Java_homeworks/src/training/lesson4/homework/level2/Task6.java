df8a8b0d9f0922a72f3f8edc55a4747af595bc3f
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-13 21:25:40 +0300

    initial commit

--- Java_homeworks/src/training/lesson4/homework/level2/Task6.java
+++ Java_homeworks/src/training/lesson4/homework/level2/Task6.java
@@ -1,8 +1,7 @@
 package training.lesson4.homework.level2;
-
 import java.util.Scanner;
 
-public class Task5 {
+public class Task6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int number1;
@@ -13,11 +12,5 @@ public class Task5 {
 
         System.out.print("Введите целое число 2: ");
         number2 = input.nextInt();
-
-        if (number1 > number2) {
-            System.out.print("Ответ: " + number1);
-        } else if (number2 > number1) {
-            System.out.print("Ответ: " + number2);
-        }
     }
 }
