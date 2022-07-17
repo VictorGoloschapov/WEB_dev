6ef7adff8c4df3c01a6d86884be0e684c1f802e4
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-17 19:15:10 +0300

    initial commit

---
+++ Java_homeworks/src/training/lesson4/homework/level4/Calculator.java
@@ -0,0 +1,19 @@
+package training.lesson4.homework.level4;
+
+public class Calculator {
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int diff(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int div(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public int mult(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+}
---
+++ Java_homeworks/src/training/lesson4/homework/level4/CalculatorTest.java
@@ -0,0 +1,4 @@
+package training.lesson4.homework.level4;
+
+public class CalculatorTest {
+}
