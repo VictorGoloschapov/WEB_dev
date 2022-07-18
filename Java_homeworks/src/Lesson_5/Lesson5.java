27892a65e14fddd7768a59e4e4496ec6719deea9
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-18 20:55:27 +0300

    initial commit

--- Java_homeworks/src/Lesson_5/Lesson5.java
+++ Java_homeworks/src/Lesson_5/Lesson5.java
@@ -1,11 +1,12 @@
 package Lesson_5;
 
+import java.util.Scanner;
 //loops while for do-while
 
 public class Lesson5 {
 
     public static void main(String[] args) {
-        int i = 0;
+        /*int i = 0;
         while (i < 5) {
             System.out.println("I = " + i);
             i++;
@@ -13,12 +14,33 @@ public class Lesson5 {
 
         for (int a = 0; a < 10; a++) {
             System.out.println("a= " + a);
+        }*/
+        Scanner input = new Scanner(System.in);
+        /*int value;
+        do {
+            System.out.println("Please enter a number");
+            value = input.nextInt();
+        } while (value != 5);
+
+        //if we got 5 continue to next app steps
+        System.out.println("got 5");*/
+
+        int i = 1;
+        if (i < 100) {
+            System.out.println("i = " + i);
+            i++;
+        }
+        if (i < 100) {
+            System.out.println("i = " + i);
+            i++;
+        }
+        if (i < 100) {
+            System.out.println("i = " + i);
+            i++;
         }
 
-        int b = 0;
-        do {
-            System.out.println("b = " + b);
-            b++;
-        } while (b < 5);
+
+
     }
+
 }
