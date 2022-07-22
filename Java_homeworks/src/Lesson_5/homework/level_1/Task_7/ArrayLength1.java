40b275ef437bb7c66d5bc4a2afad0ac921d07a0b
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 19:18:09 +0300

    initial commit

---
+++ Java_homeworks/src/Lesson_5/homework/level_1/Task_7/ArrayLength1.java
@@ -0,0 +1,12 @@
+package Lesson_5.homework.level_1.Task_7;
+
+public class ArrayLength1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[1]);
+    }
+}
+/*Решение
+1. Добавил [] после int
+2. Вызов второго элемента поменял с -1 на 1.
+ */
