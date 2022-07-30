cfb19a09cd13e5506582eb52cc3d0a9df637e367
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-30 22:23:02 +0300

    initial commit

--- Java_homeworks/src/lesson_6/level_2/Task_7/Original_code/DayOfTheWeekDetectorDemo.java
+++ Java_homeworks/src/lesson_6/level_2/Task_7/Original_code/DayOfTheWeekDetectorDemo.java
@@ -1,10 +1,21 @@
 package lesson_6.level_2.Task_7.Original_code;
 
+import java.util.Scanner;
+
 public class DayOfTheWeekDetectorDemo {
     public static void main(String[] args) {
-        int day = 1;
-
-        switch (day) {
+        /*----------------------------------*/ //1 часть программы
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7 : "); //отправить первую часть программы в отдельный метод
+        int dayNumber = scanner.nextInt();
+        /*int getDayNumberFromUser() { //метод не будет получать входные параметры
+             Scanner scanner = new Scanner(System.in);
+             System.out.print("Input day number between 1 and 7 : ");
+             int dayNumber = scanner.nextInt();
+        }
+         */
+        /*---------------------------------*/ //2 часть программы
+        switch (dayNumber) {
             case 1 -> System.out.println("Monday");
             case 2 -> System.out.println("Tuesday");
             case 3 -> System.out.println("Wednesday");
