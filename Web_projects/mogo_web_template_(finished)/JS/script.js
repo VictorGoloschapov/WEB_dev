689e9e2006beb0ae4bdbce313cc80f352f7c2ac7
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-10-26 22:07:14 +0300

    changes in directory / removed comments / removed unsed linebreaks

--- Web_projects/mogo_web_template_(finished)/JS/script.js
+++ Web_projects/mogo_web_template_(finished)/JS/script.js
@@ -2,63 +2,56 @@ $(function() {
     let header = $("#header");
     let introH  = $("#intro").innerHeight();
     let scrollOffset = $(window).scrollTop();
-    
-    /*Делаем шапку сайта фиксированой ри прокрутке сайта*/
-    //header fixed
-    checkScroll(scrollOffset);//вызов функции фиксации header
-    /*если высота scroll выше высоты блока Intro (introH), то блоку header присваивается position: fixed*/
+
+    checkScroll(scrollOffset);
     $(window).on("scroll", function() {
         scrollOffset = $(this).scrollTop();
         checkScroll(scrollOffset);
     })
-    
+
     function checkScroll(scrollOffset) {
-        /*если scrollOffset больше introH, то у header fixed*/
         if(scrollOffset >= introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
     }
-    
+
     //smooth scroll
     $("[data-scroll]").on("click", function(event) {
         event.preventDefault();
-        
+
         let $this = $(this),
-            blockId = $(this).data("scroll");//храним ID блока на который будем ссылаться при прокрутке
-        let blockOffset = $(blockId).offset().top; //сохранили позицию элемента от верха страницы
-        
-        //Придадим класс active для нажатой ссылки
+            blockId = $(this).data("scroll");
+        let blockOffset = $(blockId).offset().top;
+
         $this.addClass("active");
-        
-        //плавная прокрутка страницы
+
         $("html, body").animate({
-            scrollTop: blockOffset//казали значение, на сколько прокрутить плавно
-        }, 500/*указали длительность прокрутки в мс*/);
-        
+            scrollTop: blockOffset
+        }, 500);
+
         console.log(blockOffset);
     })
-    
+
     //Burger menu
     $("#nav-toggle").on("click", function(event) {
         event.preventDefault();
-        
+
         $(this).toggleClass("active");
         $("nav").toggleClass("active");
     });
-    
+
     //accordion
-    //следим за кликом по атрибуту
     $("[data-collapse]").on("click", function(event) {
         event.preventDefault();
-        
+
         let $this = $(this),
             blockId = $(this).data("collapse");
-        
+
         $this.toggleClass("active");
     })
-    
+
     //Slider
     $("[data-slider]").slick({
         infinite: true,
@@ -66,6 +59,4 @@ $(function() {
         slidesToShow: 1,
         slidesToScroll: 1
     });
-    
-    
 });
