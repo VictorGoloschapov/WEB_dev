e61df22d371cfa898d2723d8153ec490fd464d72
Victor_Goloschapov <victor.goloschapov@gmail.com>
2023-01-09 13:48:32 +0200

    removed pagination li from html / add pagination point logic

--- Web_projects/UI Components/Product_card_with_images/js/product.js
+++ Web_projects/UI Components/Product_card_with_images/js/product.js
@@ -1,12 +1,23 @@
 let products = document.querySelectorAll(".product");
 
+
 if (products) {
     products.forEach(el => {
-        let imageSwitchItems = el.querySelectorAll("image-switch__item");
-        let imagePagination = el.querySelectorAll(".image-pagination");
+        let imageSwitchItems = el.querySelectorAll(".image-switch__item");
+        let imagePagination = el.querySelector(".image-pagination");
 
-        imageSwitchItems.forEach((el, index) => {
-            el.setAttribute("data-index", index);
-        });
+        //delete console.log after code finish and success test
+        console.log(imageSwitchItems);
+        console.log(imagePagination);
+
+        if (imageSwitchItems.length > 1) {
+            //delete console.log after code finish and success test
+            console.log(imageSwitchItems.length);
+
+            imageSwitchItems.forEach((el, index) => {
+                el.setAttribute("data-index", index);
+                imagePagination.innerHTML += `<li class="image-pagination__item" data-index="${index}"></li>`;
+            });
+        }
     });
 }
