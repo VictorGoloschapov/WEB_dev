a026fc1101872021eff9b4a84434657c055a55a8
Victor_Goloschapov <victor.goloschapov@gmail.com>
2023-01-09 14:59:13 +0200

    script finished

--- Web_projects/UI Components/Product_card_with_images/js/product.js
+++ Web_projects/UI Components/Product_card_with_images/js/product.js
@@ -1,23 +1,48 @@
-let products = document.querySelectorAll(".product");
 
-
-if (products) {
-    products.forEach(el => {
+if (createProductItemList()) {
+    createProductItemList().forEach(el => {
+        let currentProduct = el;//current product (<article>elem</article>)
         let imageSwitchItems = el.querySelectorAll(".image-switch__item");
         let imagePagination = el.querySelector(".image-pagination");
 
-        //delete console.log after code finish and success test
-        console.log(imageSwitchItems);
-        console.log(imagePagination);
+        createImagePaginationChange(currentProduct, imageSwitchItems, imagePagination);
+    });
+}
 
-        if (imageSwitchItems.length > 1) {
-            //delete console.log after code finish and success test
-            console.log(imageSwitchItems.length);
+function createProductItemList() {
+    let products = document.querySelectorAll(".product");//nodelist of product
+    return products;
+}
 
-            imageSwitchItems.forEach((el, index) => {
-                el.setAttribute("data-index", index);
-                imagePagination.innerHTML += `<li class="image-pagination__item" data-index="${index}"></li>`;
-            });
-        }
+function createImagePaginationChange(currentProduct, imageSwitchItems, imagePagination) {
+    if (imageSwitchItems.length > 1) {
+        imageSwitchItems.forEach((el, index) => {
+            addPaginationItemToList(el, index, imagePagination);
+            makePaginationItemActive(el, currentProduct);
+            makeFirstPaginationItemActive(el, currentProduct);
+        });
+    }
+}
+
+function addPaginationItemToList(el, index, imagePagination) {
+    el.setAttribute("data-index", index);
+    imagePagination.innerHTML += `<li class="image-pagination__item ${index == 0 ? "image-pagination__item--active" : ""}" data-index="${index}"></li>`;
+}
+
+function makePaginationItemActive(el, currentProduct) {
+    el.addEventListener('mouseenter', (e) => {
+        currentProduct.querySelectorAll('.image-pagination__item').forEach(el => {
+            el.classList.remove("image-pagination__item--active");
+        });
+        currentProduct.querySelector(`.image-pagination__item[data-index="${e.currentTarget.dataset.index}"]`).classList.add("image-pagination__item--active");
+    });
+}
+
+function makeFirstPaginationItemActive(el, currentProduct) {
+    el.addEventListener('mouseleave', (e) => {
+        currentProduct.querySelectorAll('.image-pagination__item').forEach(el => {
+            el.classList.remove("image-pagination__item--active");
+        });
+        currentProduct.querySelector(`.image-pagination__item[data-index="0"]`).classList.add("image-pagination__item--active");
     });
 }
