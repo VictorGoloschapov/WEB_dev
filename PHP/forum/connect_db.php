8b5004d7c272a15867b1df7078da9cffabbc68dd
Victor_Goloschapov <victor.goloschapov@gmail.com>
2023-03-29 11:53:29 +0300

    improved db connection

--- PHP/forum/connect_db.php
+++ PHP/forum/connect_db.php
@@ -1,4 +1,13 @@
 <?php
 
-$dbc = mysqli_connect("localhost", "root", "Supertank20ss!", "forum") OR die(mysqli_connect_error());
-mysqli_set_charset($dbc, "utf-8");
+$host = "localhost";
+$userName = "root";
+$dbPassword = "Supertank20ss!";
+$dbName = "forum";
+
+$dbc = new mysqli($host, $userName, $dbPassword, $dbName);
+if ($dbc->connect_error) {
+    die("Ошибка: " . $dbc->connect_error);
+}
+echo "Подключение успешно установлено";
+$dbc->close();
