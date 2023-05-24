7f8450257b5980c3c6a571d3203d59009f2608bf
Victor_Goloschapov <victor.goloschapov@gmail.com>
2023-05-24 20:41:36 +0300

    fixes

--- PHP/car_dealer/pages/header.php
+++ PHP/car_dealer/pages/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="<?php echo $lang["nav"]["about_us"] ?>">
+<html lang="<?php echo $current_lang; ?>">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
