bf2329a2297049f49714a6dc328bb5ab1da87350
Victor_Goloschapov <victor.goloschapov@gmail.com>
2023-11-22 21:12:50 +0200

    updates

--- PHP/movieshub/register.php
+++ PHP/movieshub/register.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css">
     <title>Welcome to VicFlix</title>
 </head>
 <body>
-
+    <p>some text</p>
 </body>
 </html>
