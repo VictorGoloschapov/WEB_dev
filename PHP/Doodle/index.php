2181f62e23e83d70a294523c03b52d057b5430e5
Victor_Goloschapov <victor.goloschapov@gmail.com>
2024-05-21 20:14:32 +0300

    main page html structure

--- PHP/Doodle/index.php
+++ PHP/Doodle/index.php
@@ -1,5 +1,19 @@
-<?php
-
-echo "first line search engine project";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Doodle</title>
+</head>
+<body>
+    <div class="main_section">
+        <div class="logo_container">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="doodle_logo">
+        </div>
+        <div class="search_container">
+            <input class="search_box" type="text" name="term" placeholder="Enter search term...">
+            <input class="search_button" type="submit" value="search">
+        </div>
+    </div>
+</body>
+</html>
