b7ed11b5e47c13f6f7df72e79bfb6cd90293ce3d
Victor_Goloschapov <victor.goloschapov@gmail.com>
2024-05-21 21:54:43 +0300

    index page setup finish

--- PHP/Doodle/index.php
+++ PHP/Doodle/index.php
@@ -3,16 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/favicon//site.webmanifest">
+    <link rel="stylesheet"  type="text/css" href="assets/css/style.css">
     <title>Welcome to Doodle</title>
 </head>
 <body>
-    <div class="main_section">
-        <div class="logo_container">
-            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="doodle_logo">
-        </div>
-        <div class="search_container">
-            <input class="search_box" type="text" name="term" placeholder="Enter search term...">
-            <input class="search_button" type="submit" value="search">
+    <div class="wrapper index_page">
+        <div class="main_section">
+            <div class="logo_container">
+                <img src="assets/images/doodleLogo.png" alt="doodle_logo">
+            </div>
+            <div class="search_container">
+                <form action="search.php" method="GET">
+                    <input class="search_box" type="text" name="term" placeholder="Enter search term...">
+                    <input class="search_button" type="submit" value="Search in Doodle">
+                </form>
+            </div>
         </div>
     </div>
 </body>
