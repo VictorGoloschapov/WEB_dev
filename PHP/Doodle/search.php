9ebb7b9c0d10ef13f1f0ff9a4bcbb0cbfb8f7ffb
Victor_Goloschapov <victor.goloschapov@gmail.com>
2024-05-21 22:09:55 +0300

    search page part1

--- PHP/Doodle/search.php
+++ PHP/Doodle/search.php
@@ -1,3 +1,35 @@
-<?php
-echo $_GET["term"];
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/favicon//site.webmanifest">
+    <link rel="stylesheet"  type="text/css" href="assets/css/style.css">
+    <title>Welcome to Doodle</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="header_content">
+                <div class="logo_container">
+                    <a href="index.php"><img src="assets/images/doodleLogo.png" alt="doodle_logo"></a>
+                </div>
+                <div class="search_container">
+                    <form action="search.php" method="GET">
+                        <div class="search_bar_container">
+                            <input class="search_box" type="text" name="term">
+                            <button>
+                                Search
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
